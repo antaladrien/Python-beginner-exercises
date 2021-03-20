89f99e427ffa48e4848a6d44fa1894d54b88d5b0
Antal Adrien <43675614+antaladrien@users.noreply.github.com>
2021-03-20 10:35:12 +0100

    Add files via upload

---
+++ day-18/main.py
@@ -0,0 +1,36 @@
+# from turtle import Turtle, Screen
+#
+# tim = Turtle()
+# # timmy_the_turtle.shape("turtle")
+# # timmy_the_turtle.color("red")
+# # timmy_the_turtle.forward(100)
+# # timmy_the_turtle.right(90)
+#
+# for _ in range(4):
+#     tim.forward(100)
+#     tim.left(90)
+#
+# screen = Screen()
+# screen.exitonclick()
+
+# import turtle
+# tim = turtle.Turtle()
+
+# from turtle import Turtle
+#
+# tim = Turtle()
+# tom = Turtle()
+# terry = Turtle()
+
+# from turtle import *
+# from random import *
+
+import turtle as t
+
+tim = t.Turtle()
+
+for _ in range(15):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
