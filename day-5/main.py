a626374d63ca5973efdadae5b96609ec54c83908
Antal Adrien <43675614+antaladrien@users.noreply.github.com>
2021-03-24 19:30:16 +0100

    Add files via upload

---
+++ day-5/main.py
@@ -0,0 +1,20 @@
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " Pie")
+
+
+for number in range(1, 100):
+    print(number)
+
+for number in range(1, 101):
+    print(number)
+
+for number in range(1, 11, 3):
+    print(number)
+
+
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
