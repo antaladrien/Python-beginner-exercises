9a0a7661498267d9cee11bae71584b5435fa12a8
Antal Adrien <43675614+antaladrien@users.noreply.github.com>
2021-03-24 21:01:52 +0100

    Add files via upload

---
+++ day-10/main.py
@@ -0,0 +1,21 @@
+def format_name(f_name, l_name):
+  if f_name == "" or l_name == "":
+    return "You didn't provide valid inputs."
+  formated_f_name = f_name.title()
+  formated_l_name = l_name.title()
+  f"Result: {formated_f_name} {formated_l_name}"
+
+formatted_name = format_name(input("Your first name: "), input("Your last name: "))
+print(formatted_name)
+
+print(format_name(input("What is your first name? "), input("What is your last name? ")))
+
+length = len(formatted_name)
+
+def format_name(f_name, l_name):
+  if f_name == "" or l_name == "":
+    return "You didn't provide valid inputs."
+  formated_f_name = f_name.title()
+  formated_l_name = l_name.title()
+  return f"Result: {formated_f_name} {formated_l_name}"
+
